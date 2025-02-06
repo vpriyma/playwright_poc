3c2137b439c53b00b188ace8482b2c8d37feac00
vitalii_priyma <vip@25syv.dk>
2025-02-06 16:38:11 +0200

    integrate reporting to e2e workflow

--- playwright.config.ts
+++ playwright.config.ts
@@ -28,7 +28,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['allure-playwright'], 
+  reporter: [['allure-playwright', {outputFolder: 'test-results'}], 
     // ['html'], 
     ['line']],
 
